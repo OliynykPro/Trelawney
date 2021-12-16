309f041cf6399ad8a401ac37a252a6e4bfb97956
Taras Oliynyk <oliynykpro@Tarass-MacBook-Pro-2.local>
2021-12-16 19:23:45 +0200

    feat: create react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Col, Row } from "react-flexbox-grid";
+import "./App.scss";
 
 function App() {
+  window.contract
+    .get_price_data({ asset_ids: [] })
+    .then((res: any) => {
+      console.log("res", res);
+    })
+    .catch((err: any) => {
+      console.log("err", err);
+    });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row center="xs" className="container">
+        <Col xs={12}>Trelawney</Col>
+      </Row>
     </div>
   );
 }
---
+++ src/services/config.ts
@@ -0,0 +1,10 @@
+export default function getConfig() {
+  return {
+    networkId: "testnet",
+    nodeUrl: "https://rpc.testnet.near.org",
+    walletUrl: "https://wallet.testnet.near.org",
+    helperUrl: "https://helper.testnet.near.org",
+    explorerUrl: "https://explorer.testnet.near.org",
+    contractName: "price.rkonoval.testnet",
+  };
+}
